05f83acda8906ad9782ce80c8cd665106f50ed99
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-07-11 11:35:08 +0500

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 1: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int[] Promt(string message)
+{
+    Console.Write(message);
+    int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[" + array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.Write(", " + array[i]);
+    }
+    System.Console.Write("]");
+    System.Console.WriteLine();
+}
+
+int PositiveNumberCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int[] numbers = Promt("Введите числа через пробел: ");
+PrintArray(numbers);
+
+Console.WriteLine($"Количество чисел > 0 равно: {PositiveNumberCount(numbers)}");
